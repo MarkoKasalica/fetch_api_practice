67522b1b255cd1ec2ebbb902f4ec549bd2ca9864
MarkoKasalica <97064713+MarkoKasalica@users.noreply.github.com>
2022-02-18 03:17:46 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// ovo radi. samo mora da se prikaci na web server
+// rezultat ovog koda jeste tekst u konzoli:
+// Promise {<pending>}[[Prototype]]: Promise[[PromiseState]]: "fulfilled"[[PromiseResult]]: Response
+
+// ovaj tekst u konzoli jeste i teorijsko objasnjenje API tehnologije sa HTTP zahtevima i ostalim stvarima
+// to jos treba da pohvatam
+
+console.log(fetch("https://reqres.in/api/users"));
